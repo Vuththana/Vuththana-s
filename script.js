241f207858a0d1b176392006781e35ea721bf16f
Vuththana <135146361+Vuththana@users.noreply.github.com>
2024-01-31 16:50:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".animated-title" , {
+    strings: ["Vuththana's", "Pro Dev", "Your Babe"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
